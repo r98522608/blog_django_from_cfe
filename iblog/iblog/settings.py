4c22444b052cb46c0a1468bddcdc61713273e11f
IvesChen <r98522608@ntu.edu.tw>
2016-10-13 20:21:52 +0800

    fast patched

--- iblog/iblog/settings.py
+++ iblog/iblog/settings.py
@@ -142,10 +142,10 @@ STATICFILES_DIRS = [
             os.path.join(BASE_DIR, "media"),
                 ]
 
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"../", "static_cdn")
-# MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"../", "media_cdn")
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+# MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 
 
 import dj_database_url
